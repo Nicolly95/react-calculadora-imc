5206d937265cf5f621a33868200f20b56c5dc4b3
Nicolly95 <nicollykingeski@hotmail.com>
2026-01-30 16:20:55 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,104 @@
+"use client";
+import styles from "./page.module.css";
+import poweredImagem from "../assets/powered.png";
+import leftArrow from "../assets/leftarrow.png"
 import Image from "next/image";
+import { useState } from "react";
+import { levels, calculateImc, Level } from "@/helpers/imc";
+import { GridItem } from "@/components/GridItem";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+
+
+const Page = () => {
+
+    const [ heightField, setHeightField ] = useState<number>(0);
+    const [ weightField, setWeightField ] = useState<number>(0);
+    const [ toShow, setToShow ] = useState<Level | null>();
+
+    const handleCalculateButton = () => {
+        if(heightField && weightField) {
+            setToShow(calculateImc(heightField, weightField));
+            //'calculateImc' obtem valores para height e weight, calcula o imc,
+            // e manda o resultado para 'setToShow'
+            // 'toShow' receberá estes valores e mandará para exibição de UM GridItem - 77
+        } else {
+            alert("Digite todos os campos.");
+        }
+    }
+
+    const handleBackButton = () => {
+        setHeightField(0);
+        setWeightField(0);
+        setToShow(null);
+    }
+
+    return(
+        <div className={styles.main}>
+
+            <header className={styles.header}>
+                <div className={styles.headerContainer}>
+                    <Image src={poweredImagem} alt="" width={150} className={styles.logo}/>
+                </div>
+            </header>
+
+            <div className={styles.container}>
+
+                <div className={styles.leftSide}>
+                    <h1>Calcule o seu IMC </h1>
+                    <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial da Saúde para calcular o peso ideal de cada pessoa.</p>
+                
+                    <div className={styles.containerInputs}>
+                        <input 
+                            type="number" 
+                            placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
+                            value={heightField > 0 ? heightField : ""}
+                            onChange={event => setHeightField(parseFloat(event.target.value))}
+                            disabled={toShow ? true : false}
+                        />
+
+                        <input 
+                            type="number" 
+                            placeholder="Digite o seu peso. Ex: 70.5 (em kg)"
+                            value={weightField > 0 ? weightField : ""}
+                            onChange={event => setWeightField(parseFloat(event.target.value))}
+                            disabled={toShow ? true : false}
+                        />
+                    </div>
+
+                    <button 
+                        onClick={handleCalculateButton}
+                        disabled={toShow ? true : false}
+                    >
+                        Calcular
+                    </button>                   
+                
+                </div>
+
+
+                <div className={styles.rightSide}>
+
+                    {!toShow && 
+                        <div className={styles.grid}>
+                            {levels.map((item, key) => (
+                                <GridItem key={key} item={item} />
+                            ))}
+                        </div>
+                    }
+                    {toShow && 
+                        <div className={styles.rightBig}>
+                            <div className={styles.rightArrow} onClick={handleBackButton}>
+                                <Image src={leftArrow} alt="" width={25}/>
+                            </div>
+                            <GridItem item={toShow} />
+                        </div>
+                    }
+
+                </div>
+            </div>
+
+
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
+
+export default Page;
